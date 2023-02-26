69a9a94de83bc024dc972eff411c6a2a9540d099
ANOLASC <22795996+ANOLASC@users.noreply.github.com>
2023-02-27 00:30:28 +0800

    impl segment alloc v3

--- src/segment.rs
+++ src/segment.rs
@@ -1,8 +1,13 @@
+use std::mem::size_of;
+use std::sync::atomic::Ordering;
 use std::{ptr, sync::atomic::AtomicU32};
 
+use libc::{c_void, memset};
+use memoffset::offset_of;
+
 use crate::mimalloc_internal::mi_commit_mask_create_empty;
-use crate::mimalloc_types::MiCommitMask;
 use crate::mimalloc_types::MiOption::MiOptionEagerCommitDelay;
+use crate::mimalloc_types::{MiCommitMask, MiSlice};
 use crate::options::mi_option_is_enabled;
 use crate::{
     init::_mi_current_thread_count,
@@ -31,15 +36,13 @@ pub fn mi_segment_alloc(
     );
 
     // Commit eagerly only if not the first N lazy segments (to reduce impact of many threads that allocate just a little)
-    unsafe {
-        let eager_delay: bool = // !_mi_os_has_overcommit() &&             // never delay on overcommit systems
+    let eager_delay: bool = // !_mi_os_has_overcommit() &&             // never delay on overcommit systems
     _mi_current_thread_count() > 1 &&       // do not delay for the first N threads
-    (*tld).count < mi_option_get(MiOptionEagerCommitDelay) as u64;
+    unsafe { (*tld).count} < mi_option_get(MiOptionEagerCommitDelay) as u64;
 
-        let eager: bool = !eager_delay && mi_option_is_enabled(MiOptionEagerCommitDelay);
-        let commit = eager || (required > 0);
-        let is_zero = false;
-    }
+    let eager: bool = !eager_delay && mi_option_is_enabled(MiOptionEagerCommitDelay);
+    let commit = eager || (required > 0);
+    let is_zero = false;
 
     let mut commit_mask = MiCommitMask { mask: [0; 8] };
     let mut decommit_mask = MiCommitMask { mask: [0; 8] };
@@ -68,6 +71,43 @@ pub fn mi_segment_alloc(
     }
 
     // zero the segment info? -- not always needed as it may be zero initialized from the OS
+    unsafe {
+        (*segment)
+            .abandoned_next
+            .store(ptr::null_mut(), Ordering::Release);
+    }
+
+    if !is_zero {
+        let ofs = offset_of!(MiSegment, next);
+        let prefix = offset_of!(MiSegment, slices) - ofs;
+        unsafe {
+            memset(
+                (segment as usize + ofs) as *mut c_void,
+                0,
+                prefix + size_of::<MiSlice>() * (segment_slices + 1),
+            ); // one more
+        }
+    }
+
+    unsafe {
+        (*segment).commit_mask = commit_mask; // on lazy commit, the initial part is always committed
+                                              // (*segment).allow_decommit = mi_option_is_enabled(mi_option_allow_decommit)
+                                              //  &&!(*segment).mem_is_pinned && !(*segment).mem_is_large;
+        if (*segment).allow_decommit {
+            (*segment).decommit_expire = 0; // don't decommit just committed memory // _mi_clock_now() + mi_option_get(mi_option_decommit_delay);
+            (*segment).decommit_mask = decommit_mask;
+            // debug_assert(mi_commit_mask_all_set(&segment).commit_mask, &(*segment).decommit_mask);
+            // #if MI_DEBUG>2
+            // let commit_needed = _mi_divide_up(info_slices * MI_SEGMENT_SLICE_SIZE, MI_COMMIT_SIZE);
+            // let commit_needed_mask;
+            // mi_commit_mask_create(0, commit_needed, &commit_needed_mask);
+            // debug_assert(
+            //     !mi_commit_mask_any_set(&segment).decommit_mask,
+            //     &commit_needed_mask,
+            // );
+            // #endif
+        }
+    }
 
     ptr::null_mut()
 }
